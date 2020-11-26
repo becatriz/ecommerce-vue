bcd54781e420b89e02b4d08a742d1606e0f9d852
name <rebeca.cruz@before.com.br>
2020-11-25 23:33:45 -0400

    commit initial

---
+++ app.js
@@ -0,0 +1,73 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    carrinho: [],
+  },
+
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach((item) => {
+          total += item.preco;
+        });
+      }
+      return total;
+    },
+  },
+
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
+    },
+
+    abrirModal(id) {
+      this.fetchProdutoUnico(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+
+    fetchProdutoUnico(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+    },
+
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
+    },
+
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+    },
+
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+  },
+
+  created() {
+    this.fetchProdutos();
+  },
+});
